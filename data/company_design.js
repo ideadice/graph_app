d7b989cf3687e1a683301a8f7fc5b1b709cc8a88
yeahoni <yeahoni@gmail.com>
2019-01-29 09:20:59 +0200

    Full Data

--- data/company_design.js
+++ data/company_design.js
@@ -9,28 +9,43 @@ dollar="$"
 function set_design(cc){
     if(cc=='199018'){ 
         cc_name='foresightauto'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='257014'){ 
         cc_name='merchavia'
-        dicelinecolor='#223468'; // Line color at all charts
-        diceareacolor='rgba(34,52,104,0.46)'; // Area color at all charts
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
         dicegridcolor='#e8e8e8'; // Grid color at all charts
         document.body.style.background = "#fff"; // Background color
         currency=nis;
     }
     if(cc=='258012'){ 
         cc_name='tadiran'
-        dicelinecolor='#223468'; // Line color at all charts
-        diceareacolor='rgba(34,52,104,0.46)'; // Area color at all charts
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
         dicegridcolor='#e8e8e8'; // Grid color at all charts
         document.body.style.background = "#fff"; // Background color
         currency=nis;
     }
     if(cc=='345017'){ 
         cc_name='modiin'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='462010'){ 
         cc_name='together'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='475020'){ 
         cc_name='delek'
@@ -42,69 +57,170 @@ function set_design(cc){
     }
     if(cc=='86027'){ 
         cc_name='ybox'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='543017'){ 
         cc_name='knafaim'
-        dicelinecolor='#223468'; // Line color at all charts
-        diceareacolor='rgba(34,52,104,0.46)'; // Area color at all charts
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
         dicegridcolor='#e8e8e8'; // Grid color at all charts
         document.body.style.background = "#fff"; // Background color
         currency=nis;
     }
     if(cc=='612010'){ 
         cc_name='ildc achshara'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='612024'){ 
         cc_name='no_name'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='746016'){ 
         cc_name='strauss'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='1082007'){ 
         cc_name='shoam'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='1083443'){ 
         cc_name='internet gold'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='1083633'){ 
         cc_name='mrp'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='1090315'){ 
         cc_name='dimri'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='1094168'){ 
-        mysize}
+        cc_name='mysize'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
+
     if(cc=='1099571'){ 
         cc_name='xenia'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='1102532'){ 
         cc_name='hanan mor'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='1104058'){ 
         cc_name='tzemach'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='1105196'){ 
         cc_name='mishorim'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='1120161'){ 
         cc_name='insuline'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='1121474'){ 
         cc_name='ilda alfa'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='1129451'){ 
         cc_name='somoto'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='1140151'){ 
         cc_name='novolog'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='1141316'){ 
         cc_name='global Leasing'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='1141357'){ 
         cc_name='tamar petrolium'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
     if(cc=='1141969'){ 
         cc_name='navitas petrolium'
+        dicelinecolor='#0a75f7'; // Line color at all charts
+        diceareacolor='rgba(224,239,255, 0.2)'; // Area color at all charts
+        dicegridcolor='#e8e8e8'; // Grid color at all charts
+        document.body.style.background = "#fff"; // Background color
+        currency=nis;
     }
 }
